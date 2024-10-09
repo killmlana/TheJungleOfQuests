c1e5724cdde56018e109afc673d57a8561519a80
killmlana <mlianam@protonmail.com>
2024-10-10 03:22:08 +0530

    feat: sign-up-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {LandingComponent} from './components/landing/landing.component';
 import {SignInComponent} from './components/auth/sign-in/sign-in.component';
+import {SignUpComponent} from './components/auth/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full' },
   {path : 'home', component: LandingComponent},
   {path : 'sign-in', component: SignInComponent},
+  {path : 'sign-up', component: SignUpComponent},
 ];
