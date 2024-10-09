ba44a069d720e5aba169d1d55a15f9c470c02d8b
killmlana <mlianam@protonmail.com>
2024-10-10 02:30:36 +0530

    feat: landing-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {LandingComponent} from './components/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path : 'home', component: LandingComponent},
+];
